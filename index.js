b1c35bd46c1521740eb1467078d7920427e04baa
menomenonkwan <menomenonkwan@gmail.com>
2021-08-27 18:25:56 -0700

    built board and set up keyboard listener

---
+++ index.js
@@ -0,0 +1,105 @@
+const grid = document.querySelector('.game-grid');
+const score = document.querySelector('#score');
+const width = 28;
+let squares = [];
+
+  //28 * 28 = 784
+  // 0 - pac-dots
+  // 1 - wall
+  // 2 - ghost-lair
+  // 3 - power-pellet
+  // 4 - empty
+
+  const layout = [
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
+]
+
+function createBoard() {
+  layout.forEach(item => {
+    const square = document.createElement('div');
+
+    if(item === 0) {
+      square.classList.add('pac-dot')
+    } else if (item === 1) {
+      square.classList.add('wall')
+    } else if (item === 2) {
+      square.classList.add('lair')
+    } else if (item === 3) {
+      square.classList.add('power-pellet')
+    } else if (item === 4) {
+      square.classList.add('empty')
+    } 
+
+    grid.appendChild(square);
+    squares.push(square);
+  })
+}
+
+createBoard();
+
+
+// --------------------------------------------------
+// PACMAN
+// --------------------------------------------------
+let pacmanCurrentIndex = 490;
+
+squares[pacmanCurrentIndex].classList.add('pacman');
+
+
+// --------------------------------------------------
+// CONTROLS
+// --------------------------------------------------
+function controls(e) {
+  squares[pacmanCurrentIndex].classList.remove('pacman');
+
+  switch(e.key) {
+    case 'ArrowLeft':
+      if (pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -=1;
+      console.log('pressed left');
+      break;
+    case 'ArrowUp':
+      if (pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -=28;
+      console.log('pressed up');
+      break;
+    case 'ArrowRight':
+      if (pacmanCurrentIndex % width < width - 1) pacmanCurrentIndex +=1;
+      console.log('pressed right');
+      console.log(pacmanCurrentIndex % width);
+      break;
+    case 'ArrowDown':
+      if (pacmanCurrentIndex + width < 28**2) pacmanCurrentIndex +=28;
+      console.log('pressed down');
+      break;
+  }
+
+  squares[pacmanCurrentIndex].classList.add('pacman');
+}
+
+document.addEventListener('keyup', controls);
